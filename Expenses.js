5cdec58446c9f081e411477cbe878fdd06d713ae
lakshmiippili <116436471+lakshmiippili@users.noreply.github.com>
2023-05-16 13:15:16 +0530

    one expense list phrase added

--- Expenses.js
+++ Expenses.js
@@ -1,34 +1,27 @@
-import React,{useState} from "react";
-import ExpenseItem from "./ExpenseItem";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpenseList from "./ExpenseList";
 
 const Expenses = (props) => {
-  const [filterYear, setFilterYear] = useState('2020');
+  const [filterYear, setFilterYear] = useState("2020");
   const filterChangeHandler = (selectedYear) => {
     setFilterYear(selectedYear);
   };
 
-  const filteredExpenses = props.items.filter(exp =>{
-    return exp.date.getFullYear().toString() === filterYear
-  })
+  const filteredExpenses = props.items.filter((exp) => {
+    return exp.date.getFullYear().toString() === filterYear;
+  });
 
-      
+  
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filterYear}
         onChangeFilter={filterChangeHandler}
       />
-      {filteredExpenses.map((expense) => (
-        <ExpenseItem key={expense.id}
-          title={expense.title}
-          location={expense.location}
-          price={expense.price}
-          date={expense.date}
-        />
-        ))}
+      <ExpenseList items={filteredExpenses} />
     </Card>
   );
 };
